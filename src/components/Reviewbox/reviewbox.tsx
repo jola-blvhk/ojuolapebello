4888ad8fdeb6a7fa0098f675e558e950886fb25f
jola-blvhk <awujoolabello@gmail.com>
2024-06-28 13:21:58 +0100

    added proper width and heights to logos

--- src/components/Reviewbox/reviewbox.tsx
+++ src/components/Reviewbox/reviewbox.tsx
@@ -16,7 +16,7 @@ const ReviewBox: React.FC<ReviewBoxProps> = ({
     <div className="px-4 py-[18px] text-primary-black-90 md:px-[46px] md:py-7 rounded-[10px] border border-secondary-orange-100/30 grid gap-y-[14px] md:gap-y-4">
       <h3 className="text-sm md:text-base ">{review}</h3>
       <div className="flex gap-x-[9px]">
-        <Avvvatars value={reviewerName} style="character" size={55} />
+        <Avvvatars value={reviewerName} style="character" size={53} />
         <div className="text-sm md:text-base">
           <h4 className="font-medium text-[#2c2c2c]">{reviewerName}</h4>
           <p className=" text-[#676767]">{reviewerJobPosition}</p>
